818c12f8379ba4403c70d4eaf6f8c14b71238d4f
rhilton <rhilton@TE104-INST>
2017-01-18 13:03:17 -0700

    Add project files.

---
+++ TestOfHybrid/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestOfHybrid.MvcApplication" Language="C#" %>
---
+++ TestOfHybrid/Models/UserData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace TestOfHybrid.Models
+{
+    public class UserData
+    {
+    }
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        public int? age { get; set; }
+        public DateTime lastUpdate { get; set; }
+    }
+
+    public class UsersDbContext : DbContext
+    {
+        public DbSet<User> users { get; set; }
+    }
+}
